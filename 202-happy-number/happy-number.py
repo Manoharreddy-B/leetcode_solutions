8eda0b28a18a651047e5a2ab8786544d391570f6
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2025-03-22 15:34:10 +0900

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.52%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -3,17 +3,24 @@ class Solution:
         visit = set()
         while n not in visit:
             visit.add(n)
+            n = self.helper(n)
             if n == 1:
                return True
-            n = self.helper(n)
-
         return False
     
     def helper(self, n):
+        # total = 0
+        # n_str = str(n)
+        # for num in n_str:
+        #     total += int(num)**2
+        # return total
+
         total = 0
-        n_str = str(n)
-        for num in n_str:
-            total += int(num)**2
+        while n:
+            digit = n%10
+            digit = digit**2
+            total += digit
+            n = n//10
         return total
         
 
