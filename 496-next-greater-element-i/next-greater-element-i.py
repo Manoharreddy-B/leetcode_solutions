b56ea4204eb878fbbe44cf7702c32bb50610c637
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2025-03-22 16:54:55 +0900

    Time: 2 ms (45.79%) | Memory: 18 MB (65.65%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,23 +1,36 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # stack = []
-        # for i in range(len(nums)):
-        #     if i == 0:
-        #         stack.append(nums[i])
-        #     else:
-        #         if nums[i] > nums[i-1]:
-        #             stack.append(nums[i])
+       
+        # nums_dict = {}
+        # for i, num in enumerate(nums2):
+        #     nums_dict[num] = i
         
-        # for num in 
+        # result = [-1]*len(nums1)
+
+        # for i, num in enumerate(nums1):
+        #     for j in range(nums_dict[num],len(nums2)):
+        #         if nums2[j]>num:
+        #             result[i] = nums2[j]
+        #             break
+        # return result
+
         nums_dict = {}
-        for i, num in enumerate(nums2):
+        for i, num in enumerate(nums1):
             nums_dict[num] = i
         
         result = [-1]*len(nums1)
+        stack = []
+        for i,cur in enumerate(nums2):
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums_dict[val]
+                result[idx] = cur
 
-        for i, num in enumerate(nums1):
-            for j in range(nums_dict[num],len(nums2)):
-                if nums2[j]>num:
-                    result[i] = nums2[j]
-                    break
+            if cur in nums_dict:
+                stack.append(cur)
         return result
+
+
+
+             
+
