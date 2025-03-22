ab1e8d77886399de51662bbcf3e4363b8b2783bb
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2025-03-22 14:41:33 +0900

    Time: 27 ms (63.89%) | Memory: 29.1 MB (90.33%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,16 +1,29 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        if not nums:
-            return None
-        dic = {}
-        for num in nums:
-            if num in dic: 
-                dic[num] += 1
-                return num
-            else:
-                dic[num] = 0
+        # if not nums:
+        #     return None
+        # dic = {}
+        # for num in nums:
+        #     if num in dic: 
+        #         dic[num] += 1
+        #         return num
+        #     else:
+        #         dic[num] = 0
 
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        fast = nums[0]
+        while fast != slow:
+            slow = nums[slow]
+            fast = nums[fast]
+        return fast
 
 '''above  is not exact solution 
-exact solution uses floyd's algorithm'''
+exact solution uses floyd's algorithm
+because problem asks you to solve without using extra space'''
         
