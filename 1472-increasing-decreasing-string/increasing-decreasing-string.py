8f72be68505572bd984137f1dbcd889bb1d2837c
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2025-03-23 18:31:39 +0900

    Time: 31 ms (34.80%) | Memory: 17.8 MB (79.30%) - LeetSync

--- 1472-increasing-decreasing-string/increasing-decreasing-string.py
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.py
@@ -9,13 +9,10 @@ class Solution:
             for string in sort_arr:
                 if counter_dict[string] > 0:
                     result += string
- 
                     counter_dict[string] -= 1
-
                 else:
                     if string in counter_dict:
                         counter_dict.pop(string)
-
 
             for string in sort_arr[::-1]:
                 if counter_dict[string] > 0:
