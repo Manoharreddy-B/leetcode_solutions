39e81545db8701725f4d59580b1737c70109e45e
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2025-03-23 18:31:00 +0900

    Time: 29 ms (35.90%) | Memory: 18 MB (17.62%) - LeetSync

--- 1472-increasing-decreasing-string/increasing-decreasing-string.py
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.py
@@ -7,16 +7,12 @@ class Solution:
 
         while counter_dict:
             for string in sort_arr:
-                print(counter_dict)
                 if counter_dict[string] > 0:
                     result += string
  
                     counter_dict[string] -= 1
-            
-                # elif counter_dict[string]==0 and counter_dict[string]:
+
                 else:
-                    # if not counter_dict[string]:
-                    #     print(counter_dict, counter_dict[string])
                     if string in counter_dict:
                         counter_dict.pop(string)
 
@@ -25,7 +21,7 @@ class Solution:
                 if counter_dict[string] > 0:
                     result += string
                     counter_dict[string] -= 1
-                elif counter_dict[string]==0 and counter_dict[string]:
+                else:
                     if string in counter_dict:
                         counter_dict.pop(string)
         return result
